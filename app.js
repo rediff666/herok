8b332ceab9985b9a8e22165995eff1ccf2c06505
rediff666 <rediff666@hotmail.com>
2022-06-08 18:40:54 -0500

    A

--- app.js
+++ app.js
@@ -2,9 +2,16 @@ var http = require('http');
 var server = http.createServer();
  
 function mensaje(petic, resp) {
-	resp.writeHead(200, {'content-type': 'text/plain'});
-	resp.write('App prueba heroku');
+	// resp.writeHead(200, {'content-type': 'text/plain'});
+	// resp.write('App prueba heroku, Segunda parte agregado');
+	// resp.end();
+
+	resp.writeHead(200, {'content-type': 'text/html'});
+	resp.write("<h1 style='background-color:yellow;color:purple;'>")
+	resp.write('Parte agregada en html');
+	resp.write("</h1>")
 	resp.end();
+
 }
 server.on('request', mensaje);
  
